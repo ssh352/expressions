7d34b34e54322c8b4910ea6c7e891aa2f0f30f22
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2013-08-18 15:49:57 -0500

    print JSON COUNT properties

--- sec.r
+++ sec.r
@@ -4,6 +4,9 @@
 
 library("RJSONIO")
 
+# two JSON objects
+# con <- file("sec.write.out3.txt", "rt")
+
 con <- file("sec.write.out.txt", "rt")
 
 myRobject <- fromJSON(content=con)
@@ -11,6 +14,26 @@ print(myRobject)
 
 close(con) # always after any 'fullread'
 
+# gather properties
+
+totalRobjectsCount <- length(as.list(myRobject))
+
+print(paste("totalRobjectsCount : ",totalRobjectsCount,sep='') )
+
+if ( totalRobjectsCount > 0) {
+    totalaRobjectAttributeCount <- length(as.list(myRobject[[1]]))
+    print(paste("totalaRobjectAttributeCount : ",totalaRobjectAttributeCount,sep='') )
+}
+
+# control ( iterate though the list )
+
+# i <- 0
+# while () {
+    # print()
+    # i <- i + 1
+# }
+
+# later put put back into a better R object e.g. dataframe or matrix
 
 
 
