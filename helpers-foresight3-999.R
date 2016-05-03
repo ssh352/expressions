4452ccb56e84fd22b6cee1de87c4b20e204430c5
AndreMikulec <Andre_Mikulec@Hotmail.com>
2016-05-02 20:14:12 -0500

    aaii 4.5 hdntile move to main

--- helpers-foresight3-999.R
+++ helpers-foresight3-999.R
@@ -4,88 +4,6 @@
 
 
 
-# posneg - higher values ... higher ntiles
-hdntile  <- function(  x  = NULL                     # required: 'subset (grouped)'  dataframe
-                       , measurecol = NULL             # required:  measure column name string 
-                       , buckets = 100                  # divisions integer OR "NROW_distinct_measurecol" OR "NROW_distinct_measurecol_or_max100" strings
-                       , posneg = 1                     # 1  - higher values = higher measure, -1 - higher values lower measure  integer
-                       , new_suffix_type = "functmeas"  # new column suffix typically 'functmeas' _NTILE100 string                        , mutually exclusive of ( new_suffix_name, new_word_explicit_name )
-                       , new_suffix_expl_name = NULL    # explicity name the suffix e.g. "_MYNTILEGREAT" string                          , mutually exclusive of ( new_suffix_type, new_word_explicit_name )
-                       , new_word_explicit_name = NULL  # explicity name the the entire new column e.g. "MYQUANTILEGREAT_OF_TODAY" string , mutually exclusive of ( new_suffix_type, new_suffix_name )
-                       , monopolyhdntile  = "buckets"    # if only one firm, how does it compare against itself? 
-                         # NA - hdquantile default                  literal NA
-                         # "buckets" - use buckets hdntile default  string
-                         # "bucketsDIVtwo" use trunc(buckets/2)     string
-                         # 1,2,50,100 or any hard coded             literal integer
-                       , measurename = "NTILE"           # name the 'funct' part of 'functmeas' ( of 'measurecol'_functmeas' )            
-) {
-  
-  
-  require(Hmisc)
-  require(dplyr)
-  
-  # desire sequential integers
-  
-  if(buckets == "NROW_distinct_measurecol") {
-    buckets <- as.integer(length(unique( x[,measurecol] )))
-  }
-  
-  if(buckets == "NROW_distinct_measurecol_or_max100") {
-    buckets <- min( as.integer(length(unique( x[,measurecol] ))), 100L )
-  }
-  
-  newcolname <- ""
-  
-  # choose output column name 
-  
-  if(new_suffix_type == "functmeas") {
-    newcolname <- paste0(measurecol,"_",measurename,buckets) 
-  }
-  
-  if(!is.null(new_suffix_expl_name)) {
-    newcolname <- paste0(measurecol,"_",new_suffix_expl_name) 
-  }
-  
-  if(!is.null(new_word_explicit_name)) {
-    newcolname <- new_word_explicit_name  
-  }
-  
-  # calculate
-
-  x[[newcolname]] <-  cut((as.numeric(posneg))*x[[measurecol]], hdquantile((as.numeric(posneg))*x[[measurecol]], seq(0, 1, as.numeric(1.0/as.numeric(buckets))) , na.rm = TRUE ), labels=FALSE , include.lowest=T) 
-  
-  # pass-through  ( will be NA - hdquantile default )
-  # monopolyntilevalue <- NA
-  
-  # adjustment if only one monopoly firm exists how does it compare against itself?
-  
-  if( !is.na(monopolyhdntile) & (length(which(!is.na(x[,measurecol]))) == 1) ) {
-    
-    # browser()
-    if(  monopolyhdntile == "buckets"   ) {
-      x[,newcolname] <- ifelse(!is.na(x[,measurecol]), as.integer(buckets), NA )
-    }
-    
-    if(  monopolyhdntile == "bucketsDIVtwo"   ) {
-      x[,newcolname] <- ifelse(!is.na(x[,measurecol]), as.integer(trunc(buckets/2.0)), NA ) # pessimistic
-    }
-    
-    if(  is.numeric(monopolyhdntile)  ) {
-      x[,newcolname] <- ifelse(!is.na(x[,measurecol]), as.integer(monopolyhdntile), NA )
-    }
-    
-
-  }
-  
-  # because I do math between hdntiles
-  x[,newcolname] <- as.numeric(x[,newcolname])
-  
-  # return the data.frame
-  x
-}
-
-
-
 
 
 # posneg   higher values ... lower hdrank 
@@ -269,7 +187,7 @@ browseAAIISIPrometa <- function(searchstring = NULL ) {
   
   the_end_debug_bookmark_here <- 1
   
-  # View(META_FIELD_DESC)
+  # View(META_FIELD_DESC) 
   
 }
-# browseAAIISIPrometa("PRCHG_13W") # best deal
+# browseAAIISIPrometa("PRCHG_13W") # best deal 
