423c99b3d691675e37f7ccd47efa52306fe18130
AndreMikulec <Andre_Mikulec@Hotmail.com>
2017-01-07 22:38:54 -0600

    company, ind, sector into super df

--- finecon01.R
+++ finecon01.R
@@ -1,6 +1,6 @@
 
 
-# finecon01.R
+# finecon01.R   
 
 if(exists("copyDirectoryByPattern.default")) suppressWarnings(rm("copyDirectoryByPattern.default"))
 
@@ -200,6 +200,7 @@ verify_si_finecon_exists <- function () {
     verify_connection()
     db("create table if not exists si_finecon2();", conn.id = cid)
     db("alter table si_finecon2 add if not exists dateindex    int;", conn.id = cid)
+    db("alter table si_finecon2 add if not exists dateindexlwd int;", conn.id = cid)
     db("alter table si_finecon2 add if not exists dateindexeom int;", conn.id = cid)
     db("alter table si_finecon2 add if not exists company_id_orig  text;", conn.id = cid)
     db("alter table si_finecon2 add if not exists company_id  text;", conn.id = cid)
@@ -267,6 +268,33 @@ to.monthly.lwd <- function(x) {
   return(ret)
 }
 
+# # Thursday
+# zoo::as.Date("2016-12-29")
+# [1] "2016-12-29"
+# as.integer(zoo::as.Date("2016-12-29"))
+# [1] 17164
+
+lwd_of_month <- function(anyday = NULL) {  
+  #
+  # uses # to.monthly.lwd
+  # uses package zoo
+  #
+  seq(from = as.integer(zoo::as.Date(zoo::as.yearmon(zoo::as.Date(anyday)), frac = 0)),
+      to = as.integer(zoo::as.Date(zoo::as.yearmon(zoo::as.Date(anyday)), frac = 1)),
+      by = 1) -> all_month_days
+  as.integer(zoo::index(to.monthly.lwd(xts::xts(rep(NA_real_,length(all_month_days)),zoo::as.Date(all_month_days)))))
+}
+# lwd_of_month(17164)
+# [1] 17165
+
+last_day_of_month <- function(anyday = NULL) {  
+  # uses package zoo
+  as.integer(zoo::as.Date(zoo::as.yearmon(zoo::as.Date(anyday)), frac = 1))
+}
+#  last_day_of_month(17164)
+# [1] 17166
+
+
 
 
 # uses to.monthly.lwd, getsetvar_aaii_sipro_dir, package zoo
@@ -313,6 +341,28 @@ getvar_all_load_days_lwd <- function () {
 
 
 
+optimize <- function(tb = NULL, colz = c("dateindex","company_id")) {
+  
+  # require(data.table)
+  # require(dtplyr)        # MUST HAVE data.table
+  # require(dplyr)         # MUST HAVE
+  # require(bsearchtools)  # DOES NOT DO ARRAYS
+  
+  data.table::data.table(tb) -> tb
+  dtplyr::tbl_dt(tb, copy = FALSE) -> tb
+  data.table::setkeyv(tb, colz)
+  bsearchtools::DFI(tb,colz) -> tb
+  
+  return(tb)
+}
+
+# airquality -> aq
+# aq$company_id <- NA_character_
+# aq$dateindex <- NA_integer_
+# optimize(aq) -> aq
+
+
+
 verify_company_basics <- function (dateindex = NULL) {
   
   # R version 3.3.2 (2016-10-31) # sessionInfo()
@@ -330,27 +380,174 @@ verify_company_basics <- function (dateindex = NULL) {
     Sys.setenv(TZ="UTC")
   }
   
-  verify_company_basics_inner <- function () {
+  verify_company_basics_inner <- function (dateindex = NULL) {
     
     verify_si_finecon_exists()
     
+    require(magrittr)
+    
+    require(stringi)
+    require(stringr)
+    
+    # uses # DataCombine::MoveFront
+    # uses plyr::join
+    
+    # uses last_day_of_month
+    # uses lwd_day_of_month
+    
+    # run once
+    getvar_all_load_days_lwd_var <- getvar_all_load_days_lwd()
+    
+    ## LEFT_OFF
+    bm <- 1
+    
+    # some dateindexs in arg not found on disk
+    if(any(!dateindex %in% getvar_all_load_days_lwd_var)) { 
+      dateindex[!dateindex %in% getvar_all_load_days_lwd_var] -> dateindexes_not_found_on_disk
+      warning("one/some arg dateindex not found on disk" %s+% str_c(dateindexes_not_found_on_disk, collapse = "") )
+    }
+    
     # getsetvar_aaii_sipro_dir()
-    if(!is.null(dateindex) && (!dateindex %in% getvar_all_load_days_lwd()) ) stop("bad passsed dateindex into function verify_company_basics")
+    if(!any(dateindex %in% getvar_all_load_days_lwd_var)) stop("no arg dateindex was found on disk")
     
-    if(!is.null(dateindex) && ( dateindex %in% getvar_all_load_days_lwd())) {
+    # just the ones on found on disk    
+    # spec date(not all dates) IN 'all disk possible aaii si_pro last weekday of the month'
+    dateindex_redux <- dateindex[dateindex %in% getvar_all_load_days_lwd_var]
     
-      ## always load
-      paste0(getsetvar_aaii_sipro_dir(),"/",dateindex,"/") -> part_path_file_name
-      si_ci <- suppressWarnings(suppressMessages(foreign::read.dbf(file = paste0(part_path_file_name,"si_ci.dbf"), as.is = TRUE)))
-      # remove duplicated company_id, ticker ( will NOT be loaded into the PostgreSQL database)
+    # at least one
+    for(dateindex_redux_i in dateindex_redux) { 
+    
+      # # save for later
+      # c("setup","si_ci","si_exchg","si_mgdsc",
+      #   "si_bsq","si_isq","si_cfq",
+      #   "si_date","si_psd","si_psdc","si_psdd",
+      #   "si_mlt","si_mlt",
+      #   "si_ee")
+      
+      si_tbl <- c("si_ci","si_exchg","si_mgdsc")
+      for(si_tbl_i in si_tbl) {
+      
+        ## always load
+        paste0(getsetvar_aaii_sipro_dir(),"/",dateindex_redux_i,"/") -> part_path_file_name
+        si_si_tbl_df <- suppressWarnings(suppressMessages(foreign::read.dbf(file = paste0(part_path_file_name,si_tbl_i,".dbf"), as.is = TRUE)))
+        
+        # all lower ( PostgreSQL friendly )
+        colnames(si_si_tbl_df) <- tolower(colnames(si_si_tbl_df))
+        
+        # remove useless columns
+        # nothing starts with 'x'
+        # x_nullflags, x., x, x.1, x.2 ...
+        # 
+        si_si_tbl_df[, !str_detect(colnames(si_si_tbl_df),"^x\\.?+")   & 
+                       !str_detect(colnames(si_si_tbl_df),"^repno$")   & 
+                       !str_detect(colnames(si_si_tbl_df),"^lastmod$")
+        , drop = FALSE] -> si_si_tbl_df
+        
+        # unique ids
+        
+        if(si_tbl_i == "si_ci") {
+
+          within( si_si_tbl_df, { assign("dateindexeom", rep(last_day_of_month(dateindex_redux_i),NROW(si_si_tbl_df[,1])) )  } ) -> si_si_tbl_df
+          DataCombine::MoveFront(si_si_tbl_df,   "dateindexeom")    -> si_si_tbl_df
+          
+          within( si_si_tbl_df, { assign("dateindexlwd", rep(     lwd_of_month(dateindex_redux_i),NROW(si_si_tbl_df[,1])) )  } ) -> si_si_tbl_df
+          DataCombine::MoveFront(si_si_tbl_df,   "dateindexlwd") -> si_si_tbl_df
+          
+        }
+        
+        within( si_si_tbl_df, { assign("dateindex", rep(as.integer(dateindex_redux_i),NROW(si_si_tbl_df[,1]))      )  } ) -> si_si_tbl_df
+        DataCombine::MoveFront(si_si_tbl_df,   "dateindex")                                  -> si_si_tbl_df
+        
+        within( si_si_tbl_df, { assign("rn_" %s+% si_tbl_i, seq_along(si_si_tbl_df[,1])) } ) -> si_si_tbl_df
+        DataCombine::MoveFront(si_si_tbl_df,   "rn_" %s+% si_tbl_i)                          -> si_si_tbl_df
+        
+        # remove duplicated company_id, ticker ( will NOT be loaded into the PostgreSQL database)
+        
+        # "si_exchg","si_mgdsc" will NOT HAVE
+        if(any(colnames(si_si_tbl_df) %in% "company_id")) {
+          si_si_tbl_df[
+            !with(si_si_tbl_df, { company_id == "" | is.na(company_id) | is.null(company_id) | stri_duplicated(company_id) | stri_duplicated(company_id, fromLast = TRUE) 
+          }),,drop = FALSE] -> si_si_tbl_df
+        }
+        
+        # extra for si_ci
+        if(any(colnames(si_si_tbl_df) %in% "ticker")) {
+          si_si_tbl_df[
+            !with(si_si_tbl_df, { ticker     == "" | is.na(ticker)     | is.null(ticker)     | stri_duplicated(ticker)     | stri_duplicated(ticker    , fromLast = TRUE) 
+            }),,drop = FALSE] -> si_si_tbl_df
+        }
+          
+        # speed
+
+        # keys, DFIs, and more DFI keys: ... speed
+        if(any(colnames(si_si_tbl_df) %in% "company_id")) optimize(si_si_tbl_df)               -> si_si_tbl_df
+        if(any(colnames(si_si_tbl_df) %in% "exchg_code")) optimize(si_si_tbl_df, "exchg_code") -> si_si_tbl_df
+        if(any(colnames(si_si_tbl_df) %in% "mg_code"))    optimize(si_si_tbl_df, "mg_code")     -> si_si_tbl_df
+          
+        # si_TBL VARIABLES
+        assign(si_tbl_i,si_si_tbl_df)
+        rm(si_si_tbl_df)
+        
+      }
+      
+      # join key of exchange
+      si_exchg$x$exchg_code  -> si_exchg$x$exchange
+      
+      # outer join
+      plyr::join_all(list(si_ci$x,si_exchg$x), by = c("dateindex","exchange"), type = "full") -> si_all_df
+      
+      optimize(si_all_df) -> si_all_df
+      
+      rm(si_ci)
+      rm(si_exchg)
+      
+      # join key of industry
+      si_all_df$x$ind_3_dig -> si_all_df$x$industry_code
+       si_mgdsc$x$mg_code  ->   si_mgdsc$x$industry_code
+      
+      # left join because mg_code has codes that are sectors and not industries ( creates orphans )
+      # left join becuase orphan industries are meaningless
+      plyr::join_all(list(si_all_df$x,si_mgdsc$x), by = c("dateindex","industry_code"), type = "left") -> si_all_df
+      si_all_df$mg_desc -> si_all_df$industry_desc 
+      within( si_all_df, { rm("mg_code","mg_desc") }) -> si_all_df
+      si_all_df$rn_si_mgdsc -> si_all_df$rn_si_mgdsc_ind; within(si_all_df, { rm("rn_si_mgdsc") } ) -> si_all_df 
+      within(  si_mgdsc$x , { rm("industry_code") }) -> si_mgdsc$x
+
+      optimize(si_all_df) -> si_all_df
+      
+      # join key of sector
+      si_all_df$x$ind_2_dig            ->  si_all_df$x$sector_code
+      str_sub(si_mgdsc$x$mg_code,1,2)  ->  si_mgdsc$x$sector_code
+      
+      # get rid of duplicated sectors ( each industry has its sector rementioned )
+      si_mgdsc$x -> si_mgdsc  
+      # SPECIAL
+      si_mgdsc$rn_si_mgdsc -> si_mgdsc$rn_si_mgdsc_sect
+      si_mgdsc[with( si_mgdsc, { !stri_duplicated(sector_code) } ),,drop = FALSE] -> si_mgdsc
+        
+      optimize(si_mgdsc,c("mg_code")) -> si_mgdsc
+      
+      # left join becuase orphan sectors are meaningless
+      plyr::join_all(list(si_all_df$x,si_mgdsc$x), by = c("dateindex","sector_code"), type = "left")  -> si_all_df
+      si_all_df$mg_desc -> si_all_df$sector_desc 
+      within( si_all_df, { rm("mg_code","mg_desc") }) -> si_all_df
+      # SPECIAL 
+      within(si_all_df, { rm("rn_si_mgdsc") } ) -> si_all_df
+      within(  si_mgdsc$x , { rm("sector_code") }) -> si_mgdsc$x
+
+      optimize(si_all_df) -> si_all_df
+      
+      rm("si_mgdsc")
+      
+      bm <- 1 # LEFT_OFF 
+              # financize-like TRIM data.types ( and load into the database)
+              # ( Caroline-like & SQL: load into the database)
       
       # LEFT_OFF
       
       # look forward to NEXT month and get any NEW COMPANY_IDS by TICKER then company_id by STREET
       
-      si_mgdsc <- suppressWarnings(suppressMessages(foreign::read.dbf(file = paste0(part_path_file_name,"si_mgdsc.dbf"), as.is = TRUE)))
-      si_exchg <- suppressWarnings(suppressMessages(foreign::read.dbf(file = paste0(part_path_file_name,"si_exchg.dbf"), as.is = TRUE)))
-      
+
       # WORKS
       # colnames(si_ci)[match("EXCHANGE",colnames(si_ci))] <- "EXCHG_CODE"
       # plyr::join_all(list(si_ci,si_exchg), type = "full") 
@@ -358,18 +555,17 @@ verify_company_basics <- function (dateindex = NULL) {
       # need industry & sectors
       
       # next program ( on demand load)
-      
-      ## LEFT_OFF
-      bm <- 1
-      
-    }
+    } 
+    
+    
   }
-  verify_company_basics_inner()
+  verify_company_basics_inner(dateindex = dateindex)
   
   Sys.setenv(TZ=oldtz)
   options(ops)
 }
 # verify_company_basics(dateindex = NULL)
+# verify_company_basics(dateindex = c(15764))
 
 
 
@@ -420,9 +616,9 @@ finecon01 <- function () {
 # copyAAIISIProDBFs(
 #    from = "C:/Program Files (x86)/Stock Investor/Professional"
 #    , to   = paste0("W:/AAIISIProDBFs/",getAAIISIProDate()) # 
-#  )
+#  )            
+
+ 
 
 
-
-
-
+ 
