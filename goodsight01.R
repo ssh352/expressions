f56dc0c94c5a8c0b928031debc9462a3629ca812
AndreMikulec <Andre_Mikulec@Hotmail.com>
2017-08-12 22:17:22 -0500

    save for morning expand.xts/calculate

--- goodsight01.R
+++ goodsight01.R
@@ -247,199 +247,6 @@ PCTCHG.xts <- function(x, whiches, to_future = NULL) {
 # 2007-01-05   NA
 
 
-## [ ] SKIPPED FOR NOW # WILL COME BACK LATER ##
-# o_args is av named vector of arguments ( but user should really should use a Curry )
-# IF o_args IS OF A MIXED DATA.TAPE us a list INSTEAD ( of a vector ) =list(indexAt= 'lastof', OHLC = FALSE)
-calculate <- function(x = NULL, fnct = NULL, whiches = NULL, alt_name = NULL, o_args = NULL, prefix = NULL) {
-  
-  matched_call <- capture.output(str(match.call()))
-  
-  ops <- options()
-  
-  options(warn = 1)
-  options(width = 10000) # LIMIT # Note: set Rterm(64 bit) as appropriate
-  options(digits = 22) 
-  options(max.print=99999)
-  options(scipen=255) # Try these = width
-  
-  #correct for TZ 
-  oldtz <- Sys.getenv('TZ')
-  if(oldtz=='') {
-    Sys.setenv(TZ="UTC")
-  }
-  
-  calculate_inner <- function(x = NULL, fnct = NULL, whiches = NULL, alt_name = NULL, o_args = NULL, prefix = NULL) {
-    # uses zoo::is.zoo, zoo::as.zoo, zoo::na.locf, DescTools::DoCall, 
-    # xts:::na.locf.xts(dispatch), xts:::merge.xts(dispatch), plyr::join_all,  DataCombine::VarDrop, stringr::str_replace_all
-    # xts::is.xts, xts::as.xts, rlist::list.flatten(X?X),  rlist::list.ungroup, stringr::str_replace_all, plyr::mutate
-  
-    check_uses_packages_available("3.4.0",c("zoo","xts","rlist","stringr","DescTools","plyr","DataCombine"), matched_call)
-    
-    if(is.null(     x))  stop("run-time user must provide input data")
-    if(is.null(   fnct)) stop("run-time user must provide a function 'fnct'")
-    # if(is.null(whiches)) stop("run-time user must provide a function 'fnct' arguments: e.g. 0:4")
-    if(is.null(prefix))  prefix  <- FALSE # do  I of name fnct/alt_name do a preprend(TRUE) or append(FALSE)(default) 
-    
-    has_whiches <- TRUE     # patch # if does not have a non-null wiches argument, then give it one so it can LASTER do ONE loop
-    if(is.null(whiches)) { has_whiches <- FALSE ; whiches = -Inf }  
-  
-    have_alt_name <- FALSE
-    if(!is.null(alt_name)) have_alt_name <- TRUE # patch # but REALLY the user should use a Curry
-  
-    x -> INPUT  
-  
-    clINPUT <-  class(INPUT)[1] # typically "xts" "zoo"
-    rINPUT <-  if(xts::is.xts(INPUT) || zoo::is.zoo(INPUT)) { index(INPUT) }  else { rownames(INPUT) }
-    RETs <- lapply(whiches, function(x) {
-      lapply( INPUT, function(x,whiches) { 
-        # as.data.frame(  eval(parse(text = fnct))(x, whiches), stringsAsFactors = FALSE) -> z
-            # global
-        if(!has_whiches) whiches = NULL # send nothing             # global
-        as.data.frame( DescTools::DoCall(fnct, c(list(x), whiches, o_args, list())), stringsAsFactors = FALSE) -> z # extra list(), so c garanteed to upsize to a list
-        colnames(z) <- NULL
-        return(z) 
-      }, whiches = x ) -> out
-  
-      # Override 
-      if(have_alt_name) fnct <- alt_name
-      
-      if(has_whiches) {
-        if(prefix) { 
-          paste(            stringr::str_replace_all(fnct,"[.]|::","_"), x, names(out), sep='.') -> names(out)
-        } else { 
-          paste(names(out), stringr::str_replace_all(fnct,"[.]|::","_"), x            , sep='.') -> names(out)
-        }   
-      } else { # no whiches argument e.g. na.locf
-        if(prefix) { 
-          paste(            stringr::str_replace_all(fnct,"[.]|::","_"),    names(out), sep='.') -> names(out)
-        } else { 
-          paste(names(out), stringr::str_replace_all(fnct,"[.]|::","_")               , sep='.') -> names(out)
-        }   
-      } 
-      
-      return(out)
-    })
-    
-    # GREAT ( BUT )
-    # works well in the situation that the row.names(indexes) are ... 
-    # EXACTLY the same as INPUT ( and the same as EACH other )
-    # e.g. to.monthy will reduce the indexes
-    # 
-    # RETs <- data.frame(rlist::list.flatten(RETs))
-    
-    # make mergeable ( upgraded to data type )
-    # 2nd layer of list is promoted to the first level
-    RETsUGtDT <- lapply(rlist::list.ungroup(RETs), function(x) { 
-     
-       # zoo, xts
-      if(!is.null(indexClass(INPUT))) {
-        if(indexClass(INPUT) == "Date") {                                        # Global
-           ret <- as.xts(x, DescTools::DoCall(paste0("as.",indexClass(INPUT)),list(row.names(x)))                       )
-        } else { # could be? POSIXct, POSIXlt
-           ret <- as.xts(x, DescTools::DoCall(paste0("as.",indexClass(INPUT)),list(row.names(x))), tzone = indexTZ(INPUT))
-        }
-      } else { # something else - prepare to merge by "index"
-        ret <- plyr::mutate(x, index = row.names(x))
-      }
-      
-      return(ret)
-    
-    })
-    
-    if(any(clINPUT %in% c("xts","zoo"))) { 
-      # assign("RETs",eval(parse(text=paste0(clINPUT[1],"::","as.",clINPUT[1],"(RETs,rINPUT)")))) # xts/zoo get an extra index
-      # xts:::merge.xts(dispatch)
-      RETs <- DescTools::DoCall("merge", RETsUGtDT )
-    } else { 
-      # assign("RETs",eval(parse(text=paste0(                "as.",clINPUT[1],"(RETs)"))))
-      # rownames(RETs) <- rINPUT
-      RETs <- plyr::join_all(RETsUGtDT, by = "index", type = "full")
-      row.names(RETs) <- RETs$index
-      RETs <- RETs[!"index" %in% columns(RETs),,drop = FALSE] 
-    } 
-    
-    return(RETs)
-  }
-  
-  Sys.setenv(TZ=oldtz)
-  options(ops)
-  
-  return(calculate_inner(x = x, fnct =  fnct, whiches = whiches, alt_name = alt_name, o_args = o_args, prefix = prefix))
-
-}
-  
-# # testing
-# library(quantmod); getSymbols("IBM")
-# 
-# SMA
-#
-# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3                ),2) # default
-# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3, prefix = FALSE),2) # default
-# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3, prefix = TRUE ),2) 
-# 
-# # if xts # should dispatch on xts:::na.locf.xts
-# head(calculate(IBM, fnct = "na.locf"),2)
-#
-# na.locf
-# 
-# IBM2 <- IBM
-# IBM2[2:3,1] <- NA_real_
-# head(IBM2)
-# head(calculate(IBM2, fnct = "na.locf"),6)
-# 
-# head(calculate(IBM,  fnct = "na.locf", alt_name = "NALOCF"),2)
-#
-# lag.xts
-#
-# head(merge(IBM, calculate(IBM, fnct = "lag.xts", whiches = 1:2)))
-# 
-# PCTCHG(lag.xts)
-# 
-# head(calculate(IBM, fnct = "PCTCHG", whiches = 1),6)
-#
-# head(calculate(IBM, fnct = "PCTCHG", whiches = 1:2, alt_name = "pastPCTCHG", o_args = c(to_future = FALSE)),6)
-# tail(calculate(IBM, fnct = "PCTCHG", whiches = 1:2, alt_name = "futPCTCHG", o_args = c(to_future = TRUE)),6)
-#
-# # xts::merge.xts # dispach
-# head(merge(IBM, calculate(IBM, fnct = "TTR::SMA", whiches = 2:3)))
-# 
-
-# REDUCED index size 
-# head(calculate(IBM,  fnct = "to.monthly", o_args = list(indexAt= 'lastof', OHLC = FALSE)),6)
-
-
-# # testing 
-# library(quantmod); 
-
-# getSymbols("GDP", src = "FRED")
-
-# WEEKENDS WILL SHOW DELAYS 
-# WILL INCREASE the number of days
-# head(calculate(GDP, fnct = "delay_since_last_day.xts", alt_name = "DELAY"),6) 
-
-# > head(calculate(GDP, fnct = "delay_since_last_day.xts", alt_name = "DELAY"),10) 
-#            GDP.DELAY
-# 1947-01-01         0
-# 1947-01-02         1
-# 1947-01-03         2
-# 1947-01-04         3
-# 1947-01-05         4
-# 1947-01-06         5
-# 1947-01-07         6
-# 1947-01-08         7
-# 1947-01-09         8
-# 1947-01-10         9
-
-# What are the ways of treatng missing values in XGboost? #21
-#  Internally, XGBoost will automatically learn what is the best direction to go when a value is missing. 
-#  For continuous features, a missing(default) direction is learnt for missing value data to go into, so when the data of the speficific value is missing, then it goes to the default direction
-#  3.4 Sparsity-aware Split Finding
-#  https://arxiv.org/pdf/1603.02754.pdf
-#  10 JUN 2016
-#  XGBoost: A Scalable Tree Boosting System
-#  29 APR 2016
-# https://github.com/dmlc/xgboost/issues/21
-
 
 
 collofdays2daily.xts <- function(x) {
@@ -1428,7 +1235,200 @@ year.less.then.or.equal.xts <- function(x, n = NULL ) {
 # 1949-10-01    2
 
 
+# morning: expand.xts
 
+## [ ] SKIPPED FOR NOW # WILL COME BACK LATER ##
+# o_args is av named vector of arguments ( but user should really should use a Curry )
+# IF o_args IS OF A MIXED DATA.TAPE us a list INSTEAD ( of a vector ) =list(indexAt= 'lastof', OHLC = FALSE)
+calculate <- function(x = NULL, fnct = NULL, whiches = NULL, alt_name = NULL, o_args = NULL, prefix = NULL) {
+  
+  matched_call <- capture.output(str(match.call()))
+  
+  ops <- options()
+  
+  options(warn = 1)
+  options(width = 10000) # LIMIT # Note: set Rterm(64 bit) as appropriate
+  options(digits = 22) 
+  options(max.print=99999)
+  options(scipen=255) # Try these = width
+  
+  #correct for TZ 
+  oldtz <- Sys.getenv('TZ')
+  if(oldtz=='') {
+    Sys.setenv(TZ="UTC")
+  }
+  
+  calculate_inner <- function(x = NULL, fnct = NULL, whiches = NULL, alt_name = NULL, o_args = NULL, prefix = NULL) {
+    # uses zoo::is.zoo, zoo::as.zoo, zoo::na.locf, DescTools::DoCall, 
+    # xts:::na.locf.xts(dispatch), xts:::merge.xts(dispatch), plyr::join_all,  DataCombine::VarDrop, stringr::str_replace_all
+    # xts::is.xts, xts::as.xts, rlist::list.flatten(X?X),  rlist::list.ungroup, stringr::str_replace_all, plyr::mutate
+  
+    check_uses_packages_available("3.4.0",c("zoo","xts","rlist","stringr","DescTools","plyr","DataCombine"), matched_call)
+    
+    if(is.null(     x))  stop("run-time user must provide input data")
+    if(is.null(   fnct)) stop("run-time user must provide a function 'fnct'")
+    # if(is.null(whiches)) stop("run-time user must provide a function 'fnct' arguments: e.g. 0:4")
+    if(is.null(prefix))  prefix  <- FALSE # do  I of name fnct/alt_name do a preprend(TRUE) or append(FALSE)(default) 
+    
+    has_whiches <- TRUE     # patch # if does not have a non-null wiches argument, then give it one so it can LASTER do ONE loop
+    if(is.null(whiches)) { has_whiches <- FALSE ; whiches = -Inf }  
+  
+    have_alt_name <- FALSE
+    if(!is.null(alt_name)) have_alt_name <- TRUE # patch # but REALLY the user should use a Curry
+  
+    x -> INPUT  
+  
+    clINPUT <-  class(INPUT)[1] # typically "xts" "zoo"
+    rINPUT <-  if(xts::is.xts(INPUT) || zoo::is.zoo(INPUT)) { index(INPUT) }  else { rownames(INPUT) }
+    RETs <- lapply(whiches, function(x) {
+      lapply( INPUT, function(x,whiches) { 
+        # as.data.frame(  eval(parse(text = fnct))(x, whiches), stringsAsFactors = FALSE) -> z
+            # global
+        if(!has_whiches) whiches = NULL # send nothing             # global
+        as.data.frame( DescTools::DoCall(fnct, c(list(x), whiches, o_args, list())), stringsAsFactors = FALSE) -> z # extra list(), so c garanteed to upsize to a list
+        colnames(z) <- NULL
+        return(z) 
+      }, whiches = x ) -> out
+  
+      # Override 
+      if(have_alt_name) fnct <- alt_name
+      
+      if(has_whiches) {
+        if(prefix) { 
+          paste(            stringr::str_replace_all(fnct,"[.]|::","_"), x, names(out), sep='.') -> names(out)
+        } else { 
+          paste(names(out), stringr::str_replace_all(fnct,"[.]|::","_"), x            , sep='.') -> names(out)
+        }   
+      } else { # no whiches argument e.g. na.locf
+        if(prefix) { 
+          paste(            stringr::str_replace_all(fnct,"[.]|::","_"),    names(out), sep='.') -> names(out)
+        } else { 
+          paste(names(out), stringr::str_replace_all(fnct,"[.]|::","_")               , sep='.') -> names(out)
+        }   
+      } 
+      
+      return(out)
+    })
+    
+    # GREAT ( BUT )
+    # works well in the situation that the row.names(indexes) are ... 
+    # EXACTLY the same as INPUT ( and the same as EACH other )
+    # e.g. to.monthy will reduce the indexes
+    # 
+    # RETs <- data.frame(rlist::list.flatten(RETs))
+    
+    # make mergeable ( upgraded to data type )
+    # 2nd layer of list is promoted to the first level
+    RETsUGtDT <- lapply(rlist::list.ungroup(RETs), function(x) { 
+     
+       # zoo, xts
+      if(!is.null(indexClass(INPUT))) {
+        if(indexClass(INPUT) == "Date") {                                        # Global
+           ret <- as.xts(x, DescTools::DoCall(paste0("as.",indexClass(INPUT)),list(row.names(x)))                       )
+        } else { # could be? POSIXct, POSIXlt
+           ret <- as.xts(x, DescTools::DoCall(paste0("as.",indexClass(INPUT)),list(row.names(x))), tzone = indexTZ(INPUT))
+        }
+      } else { # something else - prepare to merge by "index"
+        ret <- plyr::mutate(x, index = row.names(x))
+      }
+      
+      return(ret)
+    
+    })
+    
+    if(any(clINPUT %in% c("xts","zoo"))) { 
+      # assign("RETs",eval(parse(text=paste0(clINPUT[1],"::","as.",clINPUT[1],"(RETs,rINPUT)")))) # xts/zoo get an extra index
+      # xts:::merge.xts(dispatch)
+      RETs <- DescTools::DoCall("merge", RETsUGtDT )
+    } else { 
+      # assign("RETs",eval(parse(text=paste0(                "as.",clINPUT[1],"(RETs)"))))
+      # rownames(RETs) <- rINPUT
+      RETs <- plyr::join_all(RETsUGtDT, by = "index", type = "full")
+      row.names(RETs) <- RETs$index
+      RETs <- RETs[!"index" %in% columns(RETs),,drop = FALSE] 
+    } 
+    
+    return(RETs)
+  }
+  
+  Sys.setenv(TZ=oldtz)
+  options(ops)
+  
+  return(calculate_inner(x = x, fnct =  fnct, whiches = whiches, alt_name = alt_name, o_args = o_args, prefix = prefix))
+
+}
+  
+# # testing
+# library(quantmod); getSymbols("IBM")
+# 
+# SMA
+#
+# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3                ),2) # default
+# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3, prefix = FALSE),2) # default
+# head(calculate(IBM, fnct = "TTR::SMA", whiches = 2:3, prefix = TRUE ),2) 
+# 
+# # if xts # should dispatch on xts:::na.locf.xts
+# head(calculate(IBM, fnct = "na.locf"),2)
+#
+# na.locf
+# 
+# IBM2 <- IBM
+# IBM2[2:3,1] <- NA_real_
+# head(IBM2)
+# head(calculate(IBM2, fnct = "na.locf"),6)
+# 
+# head(calculate(IBM,  fnct = "na.locf", alt_name = "NALOCF"),2)
+#
+# lag.xts
+#
+# head(merge(IBM, calculate(IBM, fnct = "lag.xts", whiches = 1:2)))
+# 
+# PCTCHG(lag.xts)
+# 
+# head(calculate(IBM, fnct = "PCTCHG", whiches = 1),6)
+#
+# head(calculate(IBM, fnct = "PCTCHG", whiches = 1:2, alt_name = "pastPCTCHG", o_args = c(to_future = FALSE)),6)
+# tail(calculate(IBM, fnct = "PCTCHG", whiches = 1:2, alt_name = "futPCTCHG", o_args = c(to_future = TRUE)),6)
+#
+# # xts::merge.xts # dispach
+# head(merge(IBM, calculate(IBM, fnct = "TTR::SMA", whiches = 2:3)))
+# 
+
+# REDUCED index size 
+# head(calculate(IBM,  fnct = "to.monthly", o_args = list(indexAt= 'lastof', OHLC = FALSE)),6)
+
+
+# # testing 
+# library(quantmod); 
+
+# getSymbols("GDP", src = "FRED")
+
+# WEEKENDS WILL SHOW DELAYS 
+# WILL INCREASE the number of days
+# head(calculate(GDP, fnct = "delay_since_last_day.xts", alt_name = "DELAY"),6) 
+
+# > head(calculate(GDP, fnct = "delay_since_last_day.xts", alt_name = "DELAY"),10) 
+#            GDP.DELAY
+# 1947-01-01         0
+# 1947-01-02         1
+# 1947-01-03         2
+# 1947-01-04         3
+# 1947-01-05         4
+# 1947-01-06         5
+# 1947-01-07         6
+# 1947-01-08         7
+# 1947-01-09         8
+# 1947-01-10         9
+
+# What are the ways of treatng missing values in XGboost? #21
+#  Internally, XGBoost will automatically learn what is the best direction to go when a value is missing. 
+#  For continuous features, a missing(default) direction is learnt for missing value data to go into, so when the data of the speficific value is missing, then it goes to the default direction
+#  3.4 Sparsity-aware Split Finding
+#  https://arxiv.org/pdf/1603.02754.pdf
+#  10 JUN 2016
+#  XGBoost: A Scalable Tree Boosting System
+#  29 APR 2016
+# https://github.com/dmlc/xgboost/issues/21
 
 
 
@@ -1478,6 +1478,7 @@ year.less.then.or.equal.xts <- function(x, n = NULL ) {
   #   Time series cross-validation 5
   #   January 24, 2013
   #   By Zachary Mayer ( OTHERS BY THIS AUTHOR: http://www.r-bloggers.com/author/zachary-mayer/ )
+  #   Zachary Deane-Mayer  
   #   ( CARET GUY: AND: Author of library(caretEnsemble)
   #     http://www.r-bloggers.com/time-series-cross-validation-5/
   #       http://moderntoolmaking.blogspot.com/2013/01/time-series-cross-validation-5.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+ModernToolMaking+%28Modern+Tool+Making%29
